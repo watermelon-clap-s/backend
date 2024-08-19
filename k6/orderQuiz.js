80871ab1db6f0dfdb75b0084c6a819b6731d36a2
starwook <star.wookk@gmail.com>
2024-08-19 21:02:50 +0900

    k6

---
+++ k6/orderQuiz.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+const port = 8080
+const server = "3.36.51.22"
+
+export const options = {
+    vus: 1000, // 가상 사용자 수
+    duration: '10s', // 테스트 지속 시간
+    throattle:{
+        rate: 1000/duration,
+        type: "fpm",
+    }
+};
+
+export default function () {
+    let response = http.get(server+':'+port);
+
+    // 응답 확인 (필요에 따라 수정)
+    check(response, {
+        'status was 200': (r) => r.status === 200,
+    });
+
+    sleep(1); // 각 가상 사용자가 1초 대기
+}
