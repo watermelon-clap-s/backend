8d84f0a414456083bbe4ea847db64f6169902b68
starwook <star.wookk@gmail.com>
2024-08-19 19:55:33 +0900

    folderChange

--- jmeter/scenario/startOrderQuizForServer.sh
+++ jmeter/scenario/startOrderQuizForServer.sh
@@ -1,4 +1,4 @@
 folder_name=$(date +%Y%m%d_%H%M%S)
 
 mkdir -p ../results/server/$folder_name
-../bin/jmeter -n -t ../softeer-jmx/orderEventQuizSubmitForServer.jmx -l ../results/server/$folder_name.log -e -o ../results/server/$folder_name
+../apache-jmeter-5.6.3/bin/jmeter -n -t ../softeer-jmx/orderEventQuizSubmitForServer.jmx -l ../results/server/$folder_name.log -e -o ../results/server/$folder_name
