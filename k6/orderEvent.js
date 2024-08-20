936f0e23b4a033c4c095877b592b4a431ce5ae7d
starwook <star.wookk@gmail.com>
2024-08-20 11:53:05 +0900

    wew

--- k6/orderEvent.js
+++ k6/orderEvent.js
@@ -4,18 +4,14 @@ const port = 8080
 const server = "http://3.36.51.22"
 
 export const options = {
-    vus: 100,  //천명이
+    vus: 5000,  //천명이
     duration:'2s',//2초동안
-
 };
-
 export default function () {
     let response = http.get(server+':'+port+"/event/order");
-
     // 응답 확인 (필요에 따라 수정)
     check(response, {
         'status was 200': (r) => r.status === 200,
     });
-
     sleep(1); // 각 가상 사용자가 1초 대기
 }
